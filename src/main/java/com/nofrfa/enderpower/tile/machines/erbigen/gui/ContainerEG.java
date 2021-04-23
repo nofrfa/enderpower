c58be285fea8f21beb0781995a5d5bfcb5015b17
nofrfa <72886464+nofrfa@users.noreply.github.com>
2021-04-23 14:43:20 +0300

    Update container ErbiGenerator

--- src/main/java/com/nofrfa/enderpower/tile/machines/erbigen/gui/ContainerEG.java
+++ src/main/java/com/nofrfa/enderpower/tile/machines/erbigen/gui/ContainerEG.java
@@ -2,13 +2,16 @@ package com.nofrfa.enderpower.tile.machines.erbigen.gui;
 
 import com.nofrfa.enderpower.tile.machines.erbigen.ErbiGeneratorTE;
 import ic2.core.ContainerFullInv;
+import ic2.core.slot.SlotInvSlot;
 import net.minecraft.entity.player.EntityPlayer;
 
 import java.util.List;
 
 public class ContainerEG extends ContainerFullInv<ErbiGeneratorTE> {
     public ContainerEG(EntityPlayer player, ErbiGeneratorTE tileEntity) {
-        super(player, tileEntity, 186);
+        super(player, tileEntity, 218, 256);
+        addSlotToContainer(new SlotInvSlot(tileEntity.inputFluidSlot, 0, 23, 140));
+        addSlotToContainer(new SlotInvSlot(tileEntity.outputFluidSlot, 0, 45, 140));
     }
 
     public List<String> getNetworkedFields() {
@@ -17,10 +20,12 @@ public class ContainerEG extends ContainerFullInv<ErbiGeneratorTE> {
         ret.add("tier");
         ret.add("turn");
         ret.add("stored");
+        ret.add("output");
         ret.add("maxCapacity");
         ret.add("production");
         ret.add("temperature");
         ret.add("maxTemperature");
+        ret.add("fluidTank");
         return ret;
     }
 }
